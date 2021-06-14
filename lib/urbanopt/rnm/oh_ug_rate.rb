6133658df2a0c40b742df10f41681849466b8d42
Luca de Rosa <lderosa@comillas.edu>
2021-06-14 19:58:37 +0200

    oh & ug ratio

--- lib/urbanopt/rnm/oh_ug_rate.rb
+++ lib/urbanopt/rnm/oh_ug_rate.rb
@@ -1,11 +1,38 @@
-require 'rubygems'
-require 'json'
-require 'csv'
-require 'geoutm'
+
+# *********************************************************************************
+# URBANopt (tm), Copyright (c) 2019-2020, Alliance for Sustainable Energy, LLC, and other
+# contributors. All rights reserved.
+#
+# Redistribution and use in source and binary forms, with or without modification,
+# are permitted provided that the following conditions are met:
+#
+# Redistributions of source code must retain the above copyright notice, this list
+# of conditions and the following disclaimer.
+#
+# Redistributions in binary form must reproduce the above copyright notice, this
+# list of conditions and the following disclaimer in the documentation and/or other
+# materials provided with the distribution.
+#
+# Neither the name of the copyright holder nor the names of its contributors may be
+# used to endorse or promote products derived from this software without specific
+# prior written permission.
+#
+# THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND
+# ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+# WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED.
+# IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT,
+# INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING,
+# BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
+# DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF
+# LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE
+# OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED
+# OF THE POSSIBILITY OF SUCH DAMAGE.
+# *********************************************************************************
+
 module URBANopt
     module RNM
         class Oh_ug_rate
-            attr_accessor :av_height, :type, :h_threshold, :n_buildings_street
+            attr_accessor :av_height, :type, :n_buildings_street
             def initialize()
                 @av_height = av_height
                 @type = type
@@ -68,11 +95,6 @@ module URBANopt
                 end 
                 h_threshold = h_threshold - 0.10
                 return h_threshold.to_i
-                # if (n_street_oh[ii-1] - street_set_oh).abs < (n_street_oh[ii - 2] - street_set_oh).abs
-                #     return h_threshold 
-                # else
-                #     return h_threshold - 0.2
-                # end
             end
             def classify_street_type(street_type, ug_ratio)
                 h = self.threshold_height(street_type, ug_ratio)
